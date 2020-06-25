c903fd99fcf7253060fe28ae956a5343d0c11dd4
yuriybk <email2yurii@gmail.com>
2020-06-25 23:58:34 +0300

    project init

---
+++ src/main/java/com/yuriib/springcloud/springcloud_cassandra_app/SpringcloudCassandraAppApplication.java
@@ -0,0 +1,13 @@
+package com.yuriib.springcloud.springcloud_cassandra_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudCassandraAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudCassandraAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuriib/springcloud/springcloud_cassandra_app/SpringcloudCassandraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriib.springcloud.springcloud_cassandra_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudCassandraAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
